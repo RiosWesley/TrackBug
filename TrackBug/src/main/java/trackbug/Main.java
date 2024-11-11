6a518976d96cdada80f04ffdb9d4b805f0f58b47
Wesley Rios <limarioswesley@gmail.com>
2024-11-11 17:22:46 -0300

    Icone adicionado e formularios centralizados

--- TrackBug/src/main/java/trackbug/Main.java
+++ TrackBug/src/main/java/trackbug/Main.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Carrega o ícone
+        Image icon = new Image(getClass().getResourceAsStream("/images/icon.png"));
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         Scene scene = new Scene(root, 1200, 600);
 
@@ -16,12 +20,15 @@ public class Main extends Application {
         scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
 
         primaryStage.setTitle("Login - TrackBug");
+        primaryStage.getIcons().add(icon);
         primaryStage.setScene(scene);
+        primaryStage.setMaximized(true); // Abre maximizado
         primaryStage.show();
     }
 
     public static void carregarTelaPrincipal(Stage stage) {
         try {
+            Image icon = new Image(Main.class.getResourceAsStream("/images/icon.png"));
             Parent root = FXMLLoader.load(Main.class.getResource("/fxml/main.fxml"));
             Scene scene = new Scene(root, 1200, 800);
 
@@ -29,9 +36,11 @@ public class Main extends Application {
             scene.getStylesheets().add(Main.class.getResource("/styles/styles.css").toExternalForm());
 
             stage.setTitle("TrackBug - Sistema de Gerenciamento");
+            stage.getIcons().add(icon);
             stage.setScene(scene);
             stage.setMinWidth(1000);
             stage.setMinHeight(600);
+            stage.setMaximized(true); // Abre maximizado
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
